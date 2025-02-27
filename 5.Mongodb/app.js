bcca4e06e26ab1519955926fdd5621abe18806a3
Bharath Raghavan <bharathrmofficial@gmail.com>
2025-02-27 12:05:48 +0530

    Create userMode -> 2 methods

--- 5.Mongodb/app.js
+++ 5.Mongodb/app.js
@@ -9,7 +9,43 @@ const userSchema = new mongoose.Schema({
     email : String,
     age : Number, 
     isActive : Boolean, 
-    tags : [Strings],
+    tags : [String],
     createdAt : {type : Date, default : Date.now}
 })
 
+//create a userModel
+const User = mongoose.model('User', userSchema)
+
+async function runQueryExamples() {
+    try {
+        //create a new document -> Method 1
+        const newUser = await User.create({
+            name: 'Bharath Raghavan',
+            email: 'bharathrm@gmail.com',
+            age: '35',
+            isActive: true,
+            tags: ['engineer', 'developer', 'problem-solver'],
+        })
+
+         //create a new document -> Method 2
+        //  const newUser = new User({
+        //     name: 'Nayanathara Raghavan',
+        //     email: 'nayan@gmail.com',
+        //     age: '105',
+        //     isActive: true,
+        //     tags: ['engineer', 'developer', 'problem-solver'],
+        // })
+
+        // await newUser.save();
+
+        console.log('Created a new User ', newUser);
+        
+    } catch (error) {
+        console.log('Error -> ',error)
+    }
+    finally{
+        await mongoose.connection.close();
+    }
+}
+
+runQueryExamples();
