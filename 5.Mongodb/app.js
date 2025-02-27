5665e7279d04c780bd7d1c05b7f11d6c80388ee1
Bharath Raghavan <bharathrmofficial@gmail.com>
2025-02-27 12:17:05 +0530

    Get a specific users for a choosen field

--- 5.Mongodb/app.js
+++ 5.Mongodb/app.js
@@ -19,13 +19,13 @@ const User = mongoose.model('User', userSchema)
 async function runQueryExamples() {
     try {
         //create a new document -> Method 1
-        const newUser = await User.create({
-            name: 'Bharath Raghavan',
-            email: 'bharathrm@gmail.com',
-            age: '35',
-            isActive: true,
-            tags: ['engineer', 'developer', 'problem-solver'],
-        })
+        // const newUser = await User.create({
+        //     name: 'John Doe',
+        //     email: 'johndoe@gmail.com',
+        //     age: '25',
+        //     isActive: false,
+        //     tags: ['businessman', 'developer', 'problem-solver'],
+        // })
 
          //create a new document -> Method 2
         //  const newUser = new User({
@@ -38,7 +38,17 @@ async function runQueryExamples() {
 
         // await newUser.save();
 
-        console.log('Created a new User ', newUser);
+      // console.log('Created a new User ', newUser);
+
+        //get all users
+        // const allUsers = await User.find({});
+        // console.log(allUsers);
+
+        //getting specific requirements
+        const getUserActiveFalse = await User.find({isActive : true})
+        console.log(getUserActiveFalse,);
+        
+        
         
     } catch (error) {
         console.log('Error -> ',error)
