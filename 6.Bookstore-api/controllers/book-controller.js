4791b020d863bc0a28583f61c47da5190b07fb36
Bharath Raghavan <bharathrmofficial@gmail.com>
2025-03-12 15:40:27 +0530

    added controllers and routes and then included them in server.js

---
+++ 6.Bookstore-api/controllers/book-controller.js
@@ -0,0 +1,27 @@
+const getAllBooks = async(req, res) => {
+
+}
+
+const getSingelBookByID = async (req, res) =>{
+
+}
+
+const addNewBook = async(req, res) => {
+
+}
+
+const updateBook = async(req, res) => {
+
+}
+
+const deleteBook = async(req, res) => {
+
+}
+
+module.exports = {
+    getAllBooks,
+    getSingelBookByID,
+    addNewBook,
+    updateBook,
+    deleteBook
+}
--- 6.Bookstore-api/server.js
+++ 6.Bookstore-api/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const connectToDB = require('./database/db');
+const bookRoutes = require('./routes/book-routes')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,9 @@ connectToDB();
 
 //middleware -> express.json
 app.use(express.json())
+
+//add routes here
+app.use('/api/books', bookRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is now running on PORT ${PORT}`);
